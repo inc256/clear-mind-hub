03fdde5424bbcd97361003203c1d6b6c95db1e16
inc256 <mubarakluutu20@gmail.com>
2026-05-17 02:06:32 +0300

    Thirty Sventh Commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.xplainfy.app',
   appName: 'Xplainfy',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    url: "https://app.xplainfy.net"
+  }
 };
 
 export default config;
